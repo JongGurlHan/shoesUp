a780dac5e59a0d2fc04f427fff663c11d36d46ee
JG HAN <gojkhan13@gmail.com>
2021-06-28 23:28:33 +0900

    Initial commit

---
+++ src/main/java/project/shoesUp/ShoesUpApplication.java
@@ -0,0 +1,13 @@
+package project.shoesUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/shoesUp/ShoesUpApplicationTests.java
@@ -0,0 +1,13 @@
+package project.shoesUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
