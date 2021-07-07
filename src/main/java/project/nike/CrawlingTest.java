638c0e5fd7360b365aa317b35082c8f48684bc5e
JG HAN <gojkhan13@gmail.com>
2021-07-07 23:07:25 +0900

    0707 크롤링 테스트

---
+++ src/main/java/project/nike/CrawlingTest.java
@@ -0,0 +1,30 @@
+package project.nike;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class CrawlingTest {
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("https://www.nike.com/kr/launch/?type=upcoming").get();
+        Elements el_drawMonth = doc.getElementsByAttributeValueContaining("class", "headline-4");
+        Elements el_drawDate = doc.getElementsByAttributeValueContaining("class", "headline-1");
+        Elements el_name = doc.getElementsByAttributeValueContaining("class", "headline-3");
+        Elements el_releaseTime = doc.getElementsByAttributeValueContaining("class", "headline-5");
+
+//        System.out.println(el_drawMonth.get(0).text());
+//        System.out.println(el_drawDate.get(0).text());
+//        System.out.println(el_name.get(0).text());
+//        System.out.println(el_releaseTime.get(0).text());
+
+        for(int i = 0; i<el_name.size(); i++){
+            System.out.println(el_drawMonth.get(i).text());
+            System.out.println(el_drawDate.get(i).text());
+            System.out.println(el_name.get(i).text());
+            System.out.println(el_releaseTime.get(i).text());
+            System.out.println("===================================");
+        }
+    }
+}
