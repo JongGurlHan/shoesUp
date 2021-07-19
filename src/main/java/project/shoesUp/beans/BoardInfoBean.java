c84552c52537a076f058fe1adb074df859ec992e
JG HAN <gojkhan13@gmail.com>
2021-07-19 23:17:50 +0900

    07193 Bean객체 설정

---
+++ src/main/java/project/shoesUp/beans/BoardInfoBean.java
@@ -0,0 +1,13 @@
+package project.shoesUp.beans;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoardInfoBean {
+
+    private int board_info_idx;
+    private String board_info_name;
+
+}
---
+++ src/main/java/project/shoesUp/beans/UserBean.java
@@ -0,0 +1,15 @@
+package project.shoesUp.beans;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserBean {
+
+    private int user_idx;
+    private String user_name;
+    private String user_id;
+    private String user_pw;
+
+}
